91b5a4cc2db338f73fc231fdeefaf6857ae60df0
Philipp Lackinger <Phillackinger@gmx.at>
2021-11-23 02:06:23 +0100

    🔧 Project and Config File Upadtes

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,12 @@
 import preprocess from "svelte-preprocess";
-import vercel from '@sveltejs/adapter-vercel';
+import node from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
         // hydrate the <div id="svelte"> element in src/app.html
         target: "#svelte",
-        adapter: vercel({
+        adapter: node({
             out: 'build'
         }),
     },
